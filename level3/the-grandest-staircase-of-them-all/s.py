d693b7cb7baea0f58d15869f1c7546bec81de6ea
hedy <hedy@tilde.cafe>
2022-12-02 14:45:21 +0800

    Update grandest-staircase python solution

--- level3/the-grandest-staircase-of-them-all/s.py
+++ level3/the-grandest-staircase-of-them-all/s.py
@@ -7,12 +7,32 @@
 import sys
 
 
+cache = {}
+
+# def mem(func):
+#     def wrapper(n, t, start, w):
+#         print(n, t, start, w)
+#         if f"{n},{start}" in cache.keys():
+#             return w + cache[f"{n},{start}"]
+#         w = ss6(n, t, start, w)
+#         cache[f"{n},{start}"] = w
+#         return w
+#     return wrapper
+
+
 def solution(n):
     w = ss6(n, 1, 1, 0)
     return w
 
+
+# @mem
 def ss6(n, t, start, w):
+    # print(cache)
+    # if f"{n-t},{start}" in cache.keys():
+    #     return w + cache[f"{n-t},{start}"]
+
     if start >= n or n-t <= 0 or n-t <= start:
+        cache[f"{n-t},{start}"] = w
         return w
 
     w += 1
--- level3/the-grandest-staircase-of-them-all/test.sh
+++ level3/the-grandest-staircase-of-them-all/test.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
 # TODO: test values and test time
+
+python1='python3 solution.py'
+python2='python3 s.py'
+
+for i in {1..100}; do
+    n=$((2 + $RANDOM % 200))
+    p1=$($python1 $n)
+    p2=$($python2 $n)
+
+    if [[ "$p1" != "$p2" ]]; then
+        echo "n=$n"
+        echo "solution.py: $p1"
+        echo "s.py: $p2"
+    else
+        echo -n .
+    fi
+done
+echo
