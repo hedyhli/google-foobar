b930657c50eeed7098cf69d62769012594d574fc
hedy <hedy@tilde.cafe>
2023-09-27 22:37:14 +0800

    staircase: I just had to golf it a bit you see

--- level3/the-grandest-staircase-of-them-all/solution.py
+++ level3/the-grandest-staircase-of-them-all/solution.py
@@ -1,10 +1,16 @@
+# def solution(n, t=1, s=1, w=0):
+#     if s >= n or n-t <= s:
+#         return w
+
+#     w = solution(n, t+s+1, s+1, w+1)
+#     return solution(n, t+1, s+1, w)
+
+# Sorry, I had to semi-golf XD
+# Oneliner for foobar level3, or use recursion and sacrifice time complexity?
+# => Yes
 def solution(n, t=1, s=1, w=0):
-    if s >= n or n-t <= s:
-        return w
-
-    w = solution(n, t+s+1, s+1, w+1)
-    return solution(n, t+1, s+1, w)
-
+    return w if s >= n or n-t <= s else \
+        solution(n, t+1, s+1, solution(n, t+s+1, s+1, w+1))
 
 ## THE GRANDEST STAIRCASE OF THEM ALL ##
 #
